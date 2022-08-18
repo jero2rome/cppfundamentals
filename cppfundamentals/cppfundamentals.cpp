dd006137f5206fedbb26429635449634c8ef7c28
Jerome Mohanan <jero2rome@gmail.com>
2022-08-18 12:01:48 +0200

    sticky manipulators

--- cppfundamentals/cppfundamentals.cpp
+++ cppfundamentals/cppfundamentals.cpp
@@ -1,12 +1,22 @@
-﻿// cppfundamentals.cpp : Defines the entry point for the application.
-//
-
 #include "cppfundamentals.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+  cout << endl;
+
+  // fixed is a sticky manipulator
+  cout << fixed << setprecision(2) << 12.34567;
+
+  cout << endl;
+
+  // left is a sticky manipulator, its applied to everything that follows a sticky manipulator,
+  // you need to change it back if you want to change it. Ex: Apply "right" manipulator to change to right align.
+  // right is the default setting
+  cout << left;
+  cout << setw(10) << "Spring" << setw(10) << "Nice" << endl
+    << setw(10) << "Summer" << setw(10) << "Hot";
+
+  return 0;
 }
--- cppfundamentals/cppfundamentals.h
+++ cppfundamentals/cppfundamentals.h
@@ -1,8 +1,7 @@
-﻿// cppfundamentals.h : Include file for standard system include files,
+// cppfundamentals.h : Include file for standard system include files,
 // or project specific include files.
 
 #pragma once
 
 #include <iostream>
-
-// TODO: Reference additional headers your program requires here.
+#include <iomanip>
